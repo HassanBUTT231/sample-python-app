ffeaef3bfeafe86a5757a85c6920f7043ac810ab
HassanBUTT231 <156105521+HassanBUTT231@users.noreply.github.com>
2025-05-27 15:08:12 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask Dockerized App in AWS!"
+    return "Hello from hassan Dockerized App in AWS!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
