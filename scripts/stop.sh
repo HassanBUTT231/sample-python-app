b14f4b81f7e7fcee5e29bca0e17a2a7a25110ac9
HassanBUTT231 <156105521+HassanBUTT231@users.noreply.github.com>
2025-05-27 17:29:15 +0500

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Stopping existing container..."
 
-CONTAINER_ID=$(docker ps -q --filter ancestor=099199746132.dkr.ecr.us-east-1.amazonaws.com/simple-app:latest)
+CONTAINER_ID=$(docker ps -q --filter ancestor=099199746132.dkr.ecr.us-east-1.amazonaws.com/my-app-repo:latest)
 
 if [ ! -z "$CONTAINER_ID" ]; then
   docker stop $CONTAINER_ID
