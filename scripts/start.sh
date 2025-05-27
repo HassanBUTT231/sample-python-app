416e3d463915e190dba72d96c751365abfc47c9e
HassanBUTT231 <156105521+HassanBUTT231@users.noreply.github.com>
2025-05-27 12:58:54 +0500

    add files for code deploy

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
+set -e
 
-# Variables
-AWS_REGION="us-east-1"
-IMAGE_NAME="my-app-repo"
-IMAGE_TAG="latest"
-ECR_URI="099199746132.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME"
+# Pull the Docker image from Docker Hub
+docker pull hassanbutt520/simple-app:latest
 
-# Login to ECR
-echo "Logging into Amazon ECR..."
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_URI
-
-# Pull the Docker image
-echo "Pulling Docker image from ECR..."
-docker pull $ECR_URI:$IMAGE_TAG
-
-# Run the container
-echo "Running Docker container..."
-docker run -d --name $IMAGE_NAME -p 80:5000 $ECR_URI:$IMAGE_TAG
+# Run the Docker image as a container
+docker run -d -p 5000:5000 hassanbutt520/simple-app:latest
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+set -e
 
-CONTAINER_NAME="my-app-repo"
+# Find the container ID running from the image
+CONTAINER_ID=$(docker ps -q --filter "ancestor=hassanbutt520/simple-app:latest")
 
-if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
-  echo "Stopping running container: $CONTAINER_NAME"
-  docker stop $CONTAINER_NAME
-  docker rm $CONTAINER_NAME
+if [ -n "$CONTAINER_ID" ]; then
+  echo "Stopping container $CONTAINER_ID..."
+  docker stop "$CONTAINER_ID"
+
+  echo "Removing container $CONTAINER_ID..."
+  docker rm "$CONTAINER_ID"
 else
-  echo "No running container named $CONTAINER_NAME found"
+  echo "No running container found for image hassanbutt520/simple-app:latest"
 fi
