2827b343520b7c173729db8b34c8a740d51acdb6
HassanBUTT231 <156105521+HassanBUTT231@users.noreply.github.com>
2025-05-27 15:53:53 +0500

    Create stop.sh

---
+++ scripts/stop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Stopping existing container..."
+
+CONTAINER_ID=$(docker ps -q --filter ancestor=099199746132.dkr.ecr.us-east-1.amazonaws.com/simple-app:latest)
+
+if [ ! -z "$CONTAINER_ID" ]; then
+  docker stop $CONTAINER_ID
+  docker rm $CONTAINER_ID
+else
+  echo "No running container found."
+fi
