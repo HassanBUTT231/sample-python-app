3fba4579d3829cf94d95f0d9dd3cede2851cb7a1
HassanBUTT231 <156105521+HassanBUTT231@users.noreply.github.com>
2025-05-27 15:53:15 +0500

    Update start.sh
    updated

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 set -e
-# Variables
-AWS_REGION="us-east-1"
-IMAGE_NAME="my-app-repo"
+
+# Set your values
+REGION="us-east-1"
+ACCOUNT_ID="099199746132"
+REPO_NAME="my-app-repo"
 IMAGE_TAG="latest"
-ECR_URI="099199746132.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME"
 
-# Login to ECR
 echo "Logging into Amazon ECR..."
-aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "099199746132.dkr.ecr.$AWS_REGION.amazonaws.com"
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin "$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com"
 
+echo "Pulling the Docker image..."
+docker pull "$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG"
 
-# Pull the Docker image
-echo "Pulling Docker image from ECR..."
-docker pull $ECR_URI:$IMAGE_TAG
-
-# Run the container
-echo "Running Docker container..."
-docker run -d --name $IMAGE_NAME -p 80:5000 $ECR_URI:$IMAGE_TAG
+echo "Running the Docker container..."
+docker run -d -p 5000:5000 "$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG"
