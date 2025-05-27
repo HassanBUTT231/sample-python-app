bace9d1270475cfd91ba9b9b53cb7c2636561379
HassanBUTT231 <156105521+HassanBUTT231@users.noreply.github.com>
2025-05-27 15:42:22 +0500

    Create start.sh

---
+++ scripts/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+# Variables
+AWS_REGION="us-east-1"
+IMAGE_NAME="my-app-repo"
+IMAGE_TAG="latest"
+ECR_URI="099199746132.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME"
+
+# Login to ECR
+echo "Logging into Amazon ECR..."
+aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "099199746132.dkr.ecr.$AWS_REGION.amazonaws.com"
+
+
+# Pull the Docker image
+echo "Pulling Docker image from ECR..."
+docker pull $ECR_URI:$IMAGE_TAG
+
+# Run the container
+echo "Running Docker container..."
+docker run -d --name $IMAGE_NAME -p 80:5000 $ECR_URI:$IMAGE_TAG
