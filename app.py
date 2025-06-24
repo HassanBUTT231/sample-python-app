9ba01468ac96bd827051194b4826355ecda645d5
HassanBUTT231 <156105521+HassanBUTT231@users.noreply.github.com>
2025-06-24 14:59:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Python"
+    return "Hello from Python new update"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
